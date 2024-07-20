9370b0faf1668c553483d37a1e76c5b7085d627c
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2024-07-20 17:10:02 +0100

    stuff

---
+++ lib/features/utils/constants/texts.dart
@@ -0,0 +1,6 @@
+class ConstantTexts {
+  static const String appBarTitle = 'My Location App';
+  static const String hintText = "Enter a location...";
+  static const String useCurrentLocation = "Use current Location";
+}
+
